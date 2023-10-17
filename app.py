c5ae43e0672eebbea5bfdb05cb6aa31660327734
Tim Keller <timkeller@protonmail.ch>
2023-10-18 00:56:09 +0200

    add blog overview and section to landing

--- app.py
+++ app.py
@@ -8,18 +8,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("landing_page.html")
+    recent_articles = list(articles())[-3:]
+
+    return render_template("landing_page.html", article_names=recent_articles)
 
 
-@app.route("/blog")
-def blog():
+@app.route("/blog/article")
+def blog_article():
     article_name = request.args.get('article_name')
-    print("Article: " + article_name)
-    with open(Path(__file__).parent / "static" / "blog_articles" / (article_name + ".md"), "r") as article_file:
+
+    with open(Path(__file__).parent / "static" / "blog_articles" / (article_name), "r") as article_file:
         article_string = article_file.read()
 
     article_html = markdown.markdown(article_string, extensions=['fenced_code', 'codehilite'])
     return render_template("blog_article.html", article_html=article_html)
+
+
+@app.route("/blog")
+def blog():
+
+    article_names = articles()
+
+    return render_template("blog_overview.html", article_names=article_names)
+
+
+def articles():
+    article_dir = (Path(__file__).parent / "static" / "blog_articles")
+
+    def split_name(filename: Path):
+        print(filename.name)
+        release_date, name = filename.name.split("_")
+        return release_date, name[:-3].replace("-", " "), filename
+
+    article_names = map(split_name, list(article_dir.iterdir()))
+    return article_names
 
 
 if __name__ == "__main__":
